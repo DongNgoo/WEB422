feeabe97cc03b7d7f65ad448e8db5f7e67ae40d1
DongN2 <dngo13@myseneca.ca>
2025-01-19 15:18:14 -0500

    add name and student number[Dngo13]

--- assignment1/movieAPI/server.js
+++ assignment1/movieAPI/server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Lynne Ngo Student ID: 129432233 Date: 2025-01-19
+*  Vercel Link: 
+*
+********************************************************************************/ 
+
 let express = require('express');
 let cors = require('cors');
 require('dotenv').config();
