33ccac923ddc0cd7278801536e3107ab271f29c9
DongN2 <dngo13@myseneca.ca>
2025-03-01 21:44:23 -0500

    change vercel link[Dngo13]

--- assignment2/server.js
+++ assignment2/server.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB422 – Assignment 1
+*  WEB422 – Assignment 2
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
 *  Name: Lynne Ngo Student ID: 129432233 Date: 2025-01-19
-*  Vercel Link: https://web-422-96to.vercel.app/
+*  Vercel Link: https://web-422-asgm2.vercel.app/
 *
 ********************************************************************************/ 
 const path = require('path');
